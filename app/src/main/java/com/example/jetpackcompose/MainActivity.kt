7fd3eacd3ae2f184a092a7dd9ad877ec3dccdd62
rafael-allves <rafa.viana.moc@gmail.com>
2024-07-17 11:56:14 -0300

    superscript and text customization, start to creating real interesting composable

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -10,7 +10,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.selection.DisableSelection
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,6 +21,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,7 +38,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetPackComposeTheme {
-                BoxTesting()
+                SuperScriptText("Rafael", "Alves")
             }
         }
     }
@@ -79,11 +88,60 @@ fun RowColumnTest() {
     }
 }
 
+@Composable
+fun TextTesting() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        SelectionContainer {
+            Column {
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.primary)
+                        .padding(16.dp)
+                )
+                DisableSelection {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        modifier = Modifier
+                            .background(MaterialTheme.colorScheme.primary)
+                            .padding(16.dp)
+                    )
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun SuperScriptText(
+    normalText: String,
+    superText: String
+) {
+    Text(text = buildAnnotatedString {
+        withStyle(
+            style = SpanStyle(
+                fontSize = MaterialTheme.typography.titleSmall.fontSize
+            )
+        ) {
+            append(normalText)
+        }
+        withStyle(
+            style = SpanStyle(
+                fontSize = MaterialTheme.typography.labelSmall.fontSize,
+                fontWeight = FontWeight.Normal,
+                baselineShift = BaselineShift.Superscript
+            )
+        ){
+            append(superText)
+        }
+    })
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetPackComposeTheme {
-        BoxTesting()
+        SuperScriptText("Rafael", "Alves")
     }
 }
