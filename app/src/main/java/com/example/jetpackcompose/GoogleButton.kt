ff1767d614a88aefa0b52945823f893a91b18615
rafael-allves <rafa.viana.moc@gmail.com>
2024-07-17 13:55:26 -0300

    google button just perfect, need only circular indicator

--- app/src/main/java/com/example/jetpackcompose/GoogleButton.kt
+++ app/src/main/java/com/example/jetpackcompose/GoogleButton.kt
@@ -1,15 +1,62 @@
 package com.example.jetpackcompose
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcompose.ui.theme.Shapes
 
 @Composable
-fun GoogleButton(){
-
+fun GoogleButton() {
+    val clicked = remember { mutableStateOf(false) }
+    Surface(
+        onClick = {
+            clicked.value = !clicked.value
+        },
+        shape = Shapes.medium,
+        border = BorderStroke(width = 1.dp, color = Color.LightGray),
+        color = MaterialTheme.colorScheme.surface
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(
+                    start = 12.dp,
+                    end = 16.dp,
+                    top = 12.dp,
+                    bottom = 12.dp
+                ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_google),
+                contentDescription = "Google icon",
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = stringResource(id = R.string.google_login_button))
+        }
+    }
 }
 
 @Composable
 @Preview
-private fun GoogleButtonPreview(){
+private fun GoogleButtonPreview() {
     GoogleButton()
 }
