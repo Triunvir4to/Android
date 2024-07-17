6cc7647181a7e8b7da278744e83406e1e93ea067
rafael-allves <rafa.viana.moc@gmail.com>
2024-07-17 15:01:28 -0300

    download image from github with coil

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.jetpackcompose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -22,7 +24,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -32,6 +33,7 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import com.example.jetpackcompose.ui.theme.JetPackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,15 +42,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetPackComposeTheme {
-                SocialLoginButton(
-                    loadingText = stringResource(id = R.string.login_button),
-                    icon = painterResource(id = R.drawable.ic_google),
-                    iconDescription = "Google Icon",
-                    onClick = {
-                        Log.d("Loging", "Google Login")
-                    }
-                )
+                TestCoilLib()
             }
+        }
+    }
+}
+
+@Composable
+fun TestCoilLib() {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .height(150.dp)
+                .width(150.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            val painter =
+                rememberAsyncImagePainter(model = "https://avatars.githubusercontent.com/u/75641510?v=4")
+            Image(painter = painter, contentDescription = "Github Image")
         }
     }
 }
