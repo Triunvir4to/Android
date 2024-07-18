cd453b380cb3312361c4f7d29d76766960e6daff
rafael-allves <rafa.viana.moc@gmail.com>
2024-07-18 00:46:19 -0300

    core of ProgressIndicator function done

--- app/src/main/java/com/example/jetpackcompose/components/ProgressIndicator.kt
+++ app/src/main/java/com/example/jetpackcompose/components/ProgressIndicator.kt
@@ -1,9 +1,14 @@
 package com.example.jetpackcompose.components
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
@@ -22,8 +27,25 @@ fun ProgressIndicator(
     indicatorValue: Int = 0,
     maxIndicatorValue: Int = 100,
     backgroundIndicatorColor: Color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f),
-    backGroundIndicatorStrokeWidth: Float = 100f
+    backGroundIndicatorStrokeWidth: Float = 100f,
+    foregroundIndicatorColor: Color = MaterialTheme.colorScheme.primary,
+    foregroundIndicatorStrokeWidth: Float = 100f
 ) {
+    val animatedIndicatorValue = remember { Animatable(initialValue = 0f) }
+    LaunchedEffect(key1 = indicatorValue) {
+        animatedIndicatorValue.animateTo(indicatorValue.toFloat())
+    }
+
+    val percentage =
+        (animatedIndicatorValue.value / maxIndicatorValue) * 100
+    val sweepAngle = animateFloatAsState(
+        targetValue = (2.4 * percentage).toFloat(),
+        animationSpec = tween(
+            durationMillis = 1000
+        ),
+        label = "Indicator Animation"
+    )
+
     Column(
         modifier = Modifier
             .size(canvasSize)
@@ -34,13 +56,19 @@ fun ProgressIndicator(
                     indicatorColor = backgroundIndicatorColor,
                     indicatorStrokeWidth = backGroundIndicatorStrokeWidth
                 )
+                foregroundIndicator(
+                    sweepAngle = sweepAngle.value,
+                    componentSize = indicatorSize,
+                    indicatorColor = foregroundIndicatorColor,
+                    indicatorStrokeWidth = foregroundIndicatorStrokeWidth,
+                )
             }
     ) {
 
     }
 }
 
-fun DrawScope.backgroundIndicator(
+private fun DrawScope.backgroundIndicator(
     componentSize: Size,
     indicatorColor: Color,
     indicatorStrokeWidth: Float
@@ -50,6 +78,29 @@ fun DrawScope.backgroundIndicator(
         color = indicatorColor,
         startAngle = 150f,
         sweepAngle = 240f,
+        useCenter = false,
+        style = Stroke(
+            cap = StrokeCap.Round,
+            width = indicatorStrokeWidth
+        ),
+        topLeft = Offset(
+            x = (size.width - componentSize.width) / 2,
+            y = (size.height - componentSize.height) / 2
+        )
+    )
+}
+
+private fun DrawScope.foregroundIndicator(
+    sweepAngle: Float,
+    componentSize: Size,
+    indicatorColor: Color,
+    indicatorStrokeWidth: Float
+) {
+    drawArc(
+        size = componentSize,
+        color = indicatorColor,
+        startAngle = 150f,
+        sweepAngle = sweepAngle,
         useCenter = false,
         style = Stroke(
             cap = StrokeCap.Round,
