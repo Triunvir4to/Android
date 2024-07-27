cb6a61c99ef06a353a13109b9c9277728b507cb0
rafael-allves <rafa.viana.moc@gmail.com>
2024-07-27 12:42:33 -0300

    Serial names for obj mapping

--- newsApp/app/src/main/java/com/example/newsapp/news/data/response/NewsResponse.kt
+++ newsApp/app/src/main/java/com/example/newsapp/news/data/response/NewsResponse.kt
@@ -1,7 +1,9 @@
 package com.example.newsapp.news.data.response
 
 import com.example.newsapp.news.data.model.News
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class NewsResponse(
     val available: Int,
     val news: List<News>,
