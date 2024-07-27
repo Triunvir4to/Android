4ac919a9c943ec23bcc6dd9e48d0fd8c178d81c4
rafael-allves <rafa.viana.moc@gmail.com>
2024-07-27 12:41:44 -0300

    Serial names for obj mapping

--- newsApp/app/src/main/java/com/example/newsapp/news/data/model/News.kt
+++ newsApp/app/src/main/java/com/example/newsapp/news/data/model/News.kt
@@ -1,12 +1,18 @@
 package com.example.newsapp.news.data.model
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class News(
-    val authos: List<String>,
+    val authors: List<String>,
     val id: Int,
     val image: String,
     val language: String,
+    @SerialName("publish_date")
     val publishDate: String,
     val sentiment: Double,
+    @SerialName("source_country")
     val sourceCountry: String,
     val sumary: String,
     val text: String,
