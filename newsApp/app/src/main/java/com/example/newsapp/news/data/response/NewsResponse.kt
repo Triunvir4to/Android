0c78e5307ba12ecd361bb669d64d91c7c2ace2b2
rafael-allves <rafa.viana.moc@gmail.com>
2024-07-27 12:38:48 -0300

    start news integration creating data classes for communication

--- newsApp/app/src/main/java/com/example/newsapp/news/data/response/NewsResponse.kt
+++ newsApp/app/src/main/java/com/example/newsapp/news/data/response/NewsResponse.kt
@@ -1,8 +1,10 @@
 package com.example.newsapp.news.data.response
 
+import com.example.newsapp.news.data.model.News
+
 data class NewsResponse(
     val available: Int,
-    val news: List<New>,
+    val news: List<News>,
     val number: Int,
     val offser: Int
 )
