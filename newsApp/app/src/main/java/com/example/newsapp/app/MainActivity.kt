1449de13c42a346ff4eeae6a6d723dd716684500
rafael-allves <rafa.viana.moc@gmail.com>
2024-07-27 21:52:40 -0300

    handling loading start

--- newsApp/app/src/main/java/com/example/newsapp/app/MainActivity.kt
+++ newsApp/app/src/main/java/com/example/newsapp/app/MainActivity.kt
@@ -5,12 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.newsapp.app.home.presentation.HomeScreen
 import com.example.newsapp.app.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,29 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             NewsAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
                     HomeScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewsAppTheme {
-        Greeting("Android")
     }
 }
--- newsApp/app/src/main/java/com/example/newsapp/app/home/presentation/HomeScreen.kt
+++ newsApp/app/src/main/java/com/example/newsapp/app/home/presentation/HomeScreen.kt
@@ -1,22 +1,34 @@
 package com.example.newsapp.app.home.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.newsapp.services.api.utils.ApiResponse
 import com.example.newsapp.app.news.data.model.News
 import com.example.newsapp.app.news.presentation.components.NewsItemComponent
+import com.example.newsapp.app.ui.theme.Shapes
+import com.example.newsapp.services.api.utils.ApiResponse
 
 @Composable
 private fun Loading() {
@@ -66,26 +78,69 @@ private fun Success(news: List<News>) {
 }
 
 @Composable
+private fun SearchBar(
+    text: String,
+    onSearch: (String) -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(),
+    ) {
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = text,
+            onValueChange = onSearch,
+            label = { Text(text = "Pesquisa") },
+            shape = Shapes.extraLarge,
+        )
+
+        Image(
+            painterResource(id = android.R.drawable.ic_menu_search),
+            contentDescription = "Search Icon",
+            modifier = Modifier
+                .padding(end = 16.dp)
+                .align(Alignment.CenterEnd)
+        )
+    }
+}
+
+@Composable
 fun HomeScreen() {
     val homeViewModel: HomeViewModel = hiltViewModel()
     val uiState = homeViewModel.state.collectAsState()
-
-    when (val response = uiState.value) {
-        is ApiResponse.Loading -> {
-            Loading()
+    val searchText = remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        SearchBar(text = searchText.value) {
+            searchText.value = it
         }
 
-        is ApiResponse.Fail -> {
-            val error = response.error
-            Fail(
-                error = error,
-                viewModel = homeViewModel
-            )
-        }
+        Spacer(
+            modifier = Modifier
+                .height(16.dp)
+        )
 
-        is ApiResponse.Success -> {
-            val news = response.response.body.news
-            Success(news = news)
+        when (val response = uiState.value) {
+            is ApiResponse.Loading -> {
+                Loading()
+            }
+
+            is ApiResponse.Fail -> {
+                val error = response.error
+                Fail(
+                    error = error,
+                    viewModel = homeViewModel
+                )
+            }
+
+            is ApiResponse.Success -> {
+                val news = response.response.body.news
+                Success(news = news)
+            }
         }
     }
 }
