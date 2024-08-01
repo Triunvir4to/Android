13eb3dff10fea68057783874340616a7b2bad045
rafael-allves <rafa.viana.moc@gmail.com>
2024-08-01 11:13:03 -0300

    internal database call state handling

--- newsApp/app/src/main/java/com/example/newsapp/app/news/presentation/NewsDetailsViewModel.kt
+++ newsApp/app/src/main/java/com/example/newsapp/app/news/presentation/NewsDetailsViewModel.kt
@@ -5,7 +5,10 @@ import androidx.lifecycle.viewModelScope
 import com.example.newsapp.app.news.data.dao.NewsDao
 import com.example.newsapp.app.news.data.database.NewsDatabase
 import com.example.newsapp.app.news.data.model.News
+import com.example.newsapp.services.loader.state.State
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,11 +17,20 @@ class NewsDetailsViewModel @Inject constructor(
     database: NewsDatabase
 ) : ViewModel() {
 
+    private val _state = MutableStateFlow<State<Boolean>>(State.Loading)
+    val state = _state as StateFlow<State<Boolean>>
+
     private val newsDao: NewsDao = database.newsDao()
 
-    fun addNews(news: News){
+    fun addNews(news: News) {
         viewModelScope.launch {
-            newsDao.addNews(news)
+            try {
+                _state.tryEmit(State.Loading)
+                newsDao.addNews(news)
+                _state.tryEmit(State.Success(true))
+            } catch (e: Exception) {
+                _state.tryEmit(State.Error(e.message.toString()))
+            }
         }
     }
 
